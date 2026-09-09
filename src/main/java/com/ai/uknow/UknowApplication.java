b6bffb261517cc13651adfb1f369fbfc87270ee1
satvashildesai <satvashildesai01@gmail.com>
2026-09-09 21:09:01 +0530

    Initial commit - Folder structure

---
+++ src/main/java/com/ai/uknow/UknowApplication.java
@@ -0,0 +1,13 @@
+package com.ai.uknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UknowApplication.class, args);
+	}
+
+}
